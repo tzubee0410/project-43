05f1f2b28aab90af69e56a7b6a2afdc9f9e9c0fb
tzubee0410 <59075098+tzubee0410@users.noreply.github.com>
2020-08-21 13:21:20 +0800

    .xmlhttprequest

--- js/login.js
+++ js/login.js
@@ -1,24 +1,21 @@
 //AJAX POST .xmlhttprequest
 const submit = document.querySelector('.loginbtn');
-//得到 user 資料
-
-
 
 function login(acc, paw) {
-    //將帳號存起來
-    // let user = {
-    //     email: acc,
-    //     account: paw,
-    // };
-    let data = `email=${acc}&password=${paw}`
+    //帳號密碼
+    let user = {
+        email: acc,
+        account: paw,
+    };
     let errtext = document.querySelector('.err');
     // API
     const url = 'https://hexschool-tutorial.herokuapp.com/api/signin';
     const xhr = new XMLHttpRequest();
     xhr.open('post', url, true);
     xhr.setRequestHeader("Content-type", "application/json");
-   // xhr.send(JSON.stringify(user));
-    xhr.send(data);
+    xhr.send(JSON.stringify(user));
+    console.log(user);
+    //xhr.send(user);
     xhr.onload = () => {
         let str = JSON.parse(xhr.responseText)
         if (str.success == true) {
@@ -30,9 +27,10 @@ function login(acc, paw) {
 
 }
 
-//點擊得到值
+//事件按鈕
 submit.addEventListener('click', () => {
     let acc = document.querySelector('#Account').value;
     let paw = document.querySelector('#Password').value;
     login(acc, paw);
 })
+
